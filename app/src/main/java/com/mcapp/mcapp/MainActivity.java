07082d17e87e600aa32f01f1fd6ad27f9e3bfe2c
Heron <1157842503@qq.com>
2019-10-25 11:43:59 +0800

    拍照

--- app/src/main/java/com/mcapp/mcapp/MainActivity.java
+++ app/src/main/java/com/mcapp/mcapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.mcapp.mcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Uri imgUri; //记录拍照后的照片文件的地址(临时文件)
+    private ImageView imgView;
+    private Button cameraBtn;
+    private Button galleryBtn;
+    private Button uploadBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgView=findViewById(R.id.imgView_1);
+        cameraBtn=findViewById(R.id.btn_camera);
+        galleryBtn=findViewById(R.id.btn_gallery);
+        uploadBtn=findViewById(R.id.btn_upload);
+        cameraBtn.setOnClickListener(this);
+        galleryBtn.setOnClickListener(this);
+        uploadBtn.setOnClickListener(this);
+
+    }
+
+    public void photo(View view) throws Exception{
+        //删除并创建临时文件，用于保存拍照后的照片
+        //android 6以后，写Sdcard是危险权限，需要运行时申请，但此处使用的是"关联目录"，无需！
+        File outImg=new File(getExternalCacheDir(),"temp.jpg");
+        if(outImg.exists()) outImg.delete();
+        outImg.createNewFile();
+
+        //复杂的Uri创建方式
+        if(Build.VERSION.SDK_INT>=24)
+            //这是Android 7后，更加安全的获取文件uri的方式（需要配合Provider,在Manifest.xml中加以配置）
+            imgUri= FileProvider.getUriForFile(this,"com.myapp.fileprovider",outImg);
+        else
+            imgUri= Uri.fromFile(outImg);
+
+        //利用actionName和Extra,启动《相机Activity》
+        Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imgUri);
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        switch (requestCode) {
+            case 1:
+                //此时，相机拍照完毕
+                if (resultCode == RESULT_OK) {
+                    try {
+                        //利用ContentResolver,查询临时文件，并使用BitMapFactory,从输入流中创建BitMap
+                        //同样需要配合Provider,在Manifest.xml中加以配置
+                        Bitmap map = BitmapFactory.decodeStream(getContentResolver().openInputStream(imgUri));
+                        imgView.setImageBitmap(map);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btn_camera:
+                try {
+                    photo(v);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+        }
     }
 }
